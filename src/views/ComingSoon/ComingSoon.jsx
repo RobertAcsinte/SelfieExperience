65ce46ac9960f94f89a763bb0867671643ef9003
Robert Acsinte <robert.acsinte@gmail.com>
2024-02-25 00:22:37 +0100

    added loading spinner and error message {design not implemented}

--- src/views/ComingSoon/ComingSoon.jsx
+++ src/views/ComingSoon/ComingSoon.jsx
@@ -1,38 +1,53 @@
 import styles from './ComingSoon.module.scss'
 import { ImageGallery } from "react-image-grid-gallery";
 import {useEffect, useState} from "react";
+import {PropagateLoader} from "react-spinners";
 
 
 const ComingSoon = () => {
-  const [imagesArray, setImagesArray] = useState([])
+  const [imagesArray, setImagesArray] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect( () => {
-    const loadImages = new Promise((resolve, reject) => {
-        const imageModules = import.meta.glob("../../assets/img/presentation/*");
-        const keys = Object.keys(imageModules);
-        Promise.all(keys.map(key => imageModules[key]())).then(images => {
-            resolve(images);
-        }).catch(error => {
-            reject(error)
-        })
-    })
-      loadImages.then((images) => {
-        setImagesArray(images.map((image) => (
-          {src: image.default}
-        )));
-      })
-  }, [])
+  useEffect(() => {
+          const loadImages = new Promise((resolve, reject) => {
+              const imageModules = import.meta.glob("../../assets/img/presentation/*");
+              const keys = Object.keys(imageModules);
+              Promise.all(keys.map(key => imageModules[key]())).then(images => {
+                  resolve(images);
+              }).catch(error => {
+                  reject(error);
+              })
+          });
+          loadImages.then((images) => {
+              setImagesArray(images.map((image) => (
+                  {src: image.default}
+              )));
+              setLoading(false);
+          }).catch((error) => {
+                setError(error);
+                setLoading(false);
+          });
+      },
+      []);
 
   return (
       <>
-        <div className={styles.container}>
-          Placeholder text for now, some random shit
-        </div>
-        <ImageGallery
-            imagesInfoArray={imagesArray}
-            columnWidth={230}
-            gapSize={10}
-        />
+          <div className={styles.container}>
+              <p>Placeholder text for now, some random shit</p>
+
+              <PropagateLoader
+                  color="#d63636"
+                  loading={loading}
+                  cssOverride={{"display": "inline-block"}}
+              />
+              {error && <div>error</div>}
+          </div>
+          <ImageGallery
+              imagesInfoArray={imagesArray}
+              columnWidth={230}
+              gapSize={10}
+          />
       </>
   )
 }
